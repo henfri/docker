505994be5f42e4da02ecde0f10a2283d045ba116
Hendrik Friedel <hendrik@friedels.name>
2017-02-22 20:25:01 +0100

    Update of knxd, smarthome-ng-dev added

--- knx/smarthome-ng/files/run.sh
+++ knx/smarthome-ng/files/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export PYTHONIOENCODING=utf-8
-export LANG=de_DE.UTF8
-export LC_ALL=de_DE.UTF8
-locale >> /usr/local/smarthome/var/log/locale.log
+#export PYTHONIOENCODING=utf-8
+#export LANG=de_DE.UTF8
+#export LC_ALL=de_DE.UTF8
+#locale >> /usr/local/smarthome/var/log/locale.log
 
 /usr/local/smarthome/bin/smarthome.py
 while :; do
